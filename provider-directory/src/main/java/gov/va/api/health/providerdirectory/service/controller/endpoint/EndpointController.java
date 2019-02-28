74b725351a250396109be62f226ccc7a3318d912
jjennings <joshua.jennings@libertyits.com>
2019-02-28 14:18:37 -0500

    moving private repo files to va repo

---
+++ provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/endpoint/EndpointController.java
@@ -0,0 +1,4 @@
+package gov.va.api.health.providerdirectory.service.controller.endpoint;
+
+public class EndpointController {
+}
---
+++ provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/location/LocationTransformer.java
@@ -0,0 +1,4 @@
+package gov.va.api.health.providerdirectory.service.controller.location;
+
+public class LocationTransformer {
+}
