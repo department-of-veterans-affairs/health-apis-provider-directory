862a5037942f4371c9c200e64f4ef2ac26fae41a
jjennings <joshua.jennings@libertyits.com>
2019-03-05 13:59:50 -0500

    format

--- provider-directory-api/src/main/java/gov/va/api/health/providerdirectory/api/bundle/AbstractBundle.java
+++ provider-directory-api/src/main/java/gov/va/api/health/providerdirectory/api/bundle/AbstractBundle.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
