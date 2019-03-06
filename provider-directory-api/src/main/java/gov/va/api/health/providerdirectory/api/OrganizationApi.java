11237458e27aa7d04e0501473d6709e0b0eeed61
jjennings <joshua.jennings@libertyits.com>
2019-03-06 13:30:03 -0500

    unnecessary import

--- provider-directory-api/src/main/java/gov/va/api/health/providerdirectory/api/OrganizationApi.java
+++ provider-directory-api/src/main/java/gov/va/api/health/providerdirectory/api/OrganizationApi.java
@@ -1,6 +1,5 @@
 package gov.va.api.health.providerdirectory.api;
 
-import gov.va.api.health.providerdirectory.api.datatypes.Identifier;
 import gov.va.api.health.providerdirectory.api.resources.OperationOutcome;
 import gov.va.api.health.providerdirectory.api.resources.Organization;
 import io.swagger.v3.oas.annotations.Operation;
