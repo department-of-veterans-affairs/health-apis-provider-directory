9906cc54ead8109b8fdfecaf380485f530db5bae
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-07-23 09:58:21 -0400

    Inline test data.

--- provider-directory/src/test/java/gov/va/api/health/providerdirectory/service/controller/location/LocationControllerTest.java
+++ provider-directory/src/test/java/gov/va/api/health/providerdirectory/service/controller/location/LocationControllerTest.java
@@ -1,22 +1,24 @@
 package gov.va.api.health.providerdirectory.service.controller.location;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.Iterables;
 import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.providerdirectory.service.CareSitesResponse;
 import gov.va.api.health.providerdirectory.service.ProviderServicesResponse;
 import gov.va.api.health.providerdirectory.service.client.PpmsClient;
 import gov.va.api.health.providerdirectory.service.controller.Bundler;
 import gov.va.api.health.providerdirectory.service.controller.ConfigurableBaseUrlPageLinks;
+import gov.va.api.health.stu3.api.datatypes.ContactPoint;
 import gov.va.api.health.stu3.api.resources.Location;
 import lombok.SneakyThrows;
 import org.junit.Test;
 
 @SuppressWarnings("WeakerAccess")
 public final class LocationControllerTest {
-
   LocationController.Transformer tx = new LocationTransformer();
 
   ConfigurableBaseUrlPageLinks configurableBaseUrlPageLinks =
@@ -69,14 +71,31 @@ public final class LocationControllerTest {
   @Test
   @SneakyThrows
   public void searchByIdentifier() {
-    ProviderServicesResponse response =
-        JacksonConfig.createMapper()
-            .readValue(
-                getClass()
-                    .getResourceAsStream(
-                        "/LocationTestResource/mock-provider-services-response.json"),
-                ProviderServicesResponse.class);
-    when(ppmsClient.providerServicesById("123")).thenReturn(response);
+    when(ppmsClient.providerServicesById("123"))
+        .thenReturn(
+            ProviderServicesResponse.builder()
+                .value(
+                    asList(
+                        ProviderServicesResponse.Value.builder()
+                            .name("A I Advance Imaging of Tulsa LLC - Clinic/Center - Radiology ")
+                            .affiliationName("TriWest - Choice")
+                            .relationshipName("Choice")
+                            .providerName("A I Advance Imaging of Tulsa LLC")
+                            .specialtyName("Clinic/Center - Radiology")
+                            .hpp("Unknown")
+                            .highPerformingProvider("TriWest - Choice(U)")
+                            .careSiteName("A I Advance Imaging of Tulsa LLC")
+                            .careSiteLocationAddress("6711 S Yale Ave Ste 212, Tulsa, OK, 74136")
+                            .careSiteAddressStreet("6711 S Yale Ave Ste 212")
+                            .careSiteAddressCity("Tulsa")
+                            .careSiteAddressState("OK")
+                            .careSiteAddressZipCode("74136")
+                            .latitude("36.066014")
+                            .longitude("-95.921907")
+                            .careSitePhoneNumber("9185230002")
+                            .organiztionGroupName("A I Advance Imaging of Tulsa LLC")
+                            .build()))
+                .build());
     Location.Bundle expected = controller.searchByIdentifier("123", 1, 15);
     Location.Bundle actual =
         JacksonConfig.createMapper()
@@ -91,24 +110,55 @@ public final class LocationControllerTest {
   @Test
   @SneakyThrows
   public void searchByName() {
-    ProviderServicesResponse response =
-        JacksonConfig.createMapper()
-            .readValue(
-                getClass()
-                    .getResourceAsStream(
-                        "/LocationTestResource/mock-provider-services-response.json"),
-                ProviderServicesResponse.class);
     when(ppmsClient.providerServicesByName("A I Advance Imaging of Tulsa LLC"))
-        .thenReturn(response);
-    Location.Bundle expected = controller.searchByName("A I Advance Imaging of Tulsa LLC", 1, 15);
-    Location.Bundle actual =
-        JacksonConfig.createMapper()
-            .readValue(
-                getClass()
-                    .getResourceAsStream(
-                        "/LocationTestResource/expected-search-location-by-name.json"),
-                Location.Bundle.class);
-    assertThat(actual).isEqualTo(expected);
+        .thenReturn(
+            ProviderServicesResponse.builder()
+                .value(
+                    asList(
+                        ProviderServicesResponse.Value.builder()
+                            .name("A I Advance Imaging of Tulsa LLC - Clinic/Center - Radiology ")
+                            .affiliationName("TriWest - Choice")
+                            .relationshipName("Choice")
+                            .providerName("A I Advance Imaging of Tulsa LLC")
+                            .specialtyName("Clinic/Center - Radiology")
+                            .hpp("Unknown")
+                            .highPerformingProvider("TriWest - Choice(U)")
+                            .careSiteName("A I Advance Imaging of Tulsa LLC")
+                            .careSiteLocationAddress("6711 S Yale Ave Ste 212, Tulsa, OK, 74136")
+                            .careSiteAddressStreet("6711 S Yale Ave Ste 212")
+                            .careSiteAddressCity("Tulsa")
+                            .careSiteAddressState("OK")
+                            .careSiteAddressZipCode("74136")
+                            .latitude("36.066014")
+                            .longitude("-95.921907")
+                            .careSitePhoneNumber("9185230002")
+                            .organiztionGroupName("A I Advance Imaging of Tulsa LLC")
+                            .build()))
+                .build());
+
+    Location.Bundle actual = controller.searchByName("A I Advance Imaging of Tulsa LLC", 1, 15);
+
+    assertThat(Iterables.getOnlyElement(actual.entry()).resource())
+        .isEqualTo(
+            Location.builder()
+                .resourceType("Location")
+                .status(Location.Status.active)
+                .name("A I Advance Imaging of Tulsa LLC")
+                .telecom(
+                    asList(
+                        ContactPoint.builder()
+                            .system(ContactPoint.ContactPointSystem.phone)
+                            .value("9185230002")
+                            .build()))
+                .address(
+                    Location.LocationAddress.builder()
+                        .text("6711 S Yale Ave Ste 212, Tulsa, OK, 74136")
+                        .line(asList("6711 S Yale Ave Ste 212"))
+                        .city("Tulsa")
+                        .state("OK")
+                        .postalCode("74136")
+                        .build())
+                .build());
   }
 
   @Test
