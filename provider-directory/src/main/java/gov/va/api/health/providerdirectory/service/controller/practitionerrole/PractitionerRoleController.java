f76d9c585409635e5ecaa02be6f303ed2428c75f
jnguy <jennifer.nguy@libertyits.com>
2019-08-08 10:53:47 -0400

    PractitionerRole count and paging

--- provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/practitionerrole/PractitionerRoleController.java
+++ provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/practitionerrole/PractitionerRoleController.java
@@ -10,9 +10,11 @@ import gov.va.api.health.providerdirectory.service.controller.PageLinks.LinkConf
 import gov.va.api.health.providerdirectory.service.controller.Parameters;
 import gov.va.api.health.stu3.api.resources.PractitionerRole;
 import java.util.ArrayList;
-import java.util.Collections;
+import org.apache.commons.lang3.tuple.Pair;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import javax.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -25,6 +27,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Request Mappings for Practitioner Role Profile, see
@@ -49,69 +53,124 @@ public class PractitionerRoleController {
 
   private PractitionerRole.Bundle bundle(
       MultiValueMap<String, String> parameters, int page, int count) {
-    PractitionerRoleWrapper root = search(parameters);
+    Pair<List<PractitionerRoleWrapper>, Integer> root = search(parameters);
     LinkConfig linkConfig =
         LinkConfig.builder()
             .path("PractitionerRole")
             .queryParams(parameters)
             .page(page)
             .recordsPerPage(count)
-            .totalRecords(1)
+            .totalRecords(root.getRight())
             .build();
     return bundler.bundle(
         BundleContext.of(
             linkConfig,
-            Collections.singletonList(root),
+            root.getLeft(),
             transformer,
             PractitionerRole.Entry::new,
             PractitionerRole.Bundle::new));
   }
 
+  /** Builds the PractitionerWrapper and returns it. */
+  private PractitionerRoleWrapper practitionerRoleWrapperBuilder(
+          ProviderContactsResponse providerContactsResponse, ProviderResponse.Value providerResponse, ProviderSpecialtiesResponse providerSpecialtiesResponse) {
+    PractitionerRoleWrapper.PractitionerRoleWrapperBuilder filteredResults =
+            new PractitionerRoleWrapper.PractitionerRoleWrapperBuilder();
+    filteredResults
+            .providerResponse(ProviderResponse.builder().value(singletonList(providerResponse)).build())
+            .providerContactsResponse(providerContactsResponse)
+            .providerSpecialtiesResponse(providerSpecialtiesResponse);
+    return filteredResults.build();
+  }
+
   /** Read by identifier. */
   @GetMapping(value = {"/{publicId}"})
   public PractitionerRole readByIdentifier(@PathVariable("publicId") String publicId) {
-    return transformer.apply(search(Parameters.forIdentity((publicId))));
+    return transformer.apply(search(Parameters.forIdentity((publicId))).getKey().get(0));
   }
 
-  private PractitionerRoleWrapper search(MultiValueMap<String, String> parameters) {
-    ProviderResponse providerResponse;
-    if (parameters.containsKey("identifier")) {
-      String identifier = parameters.getFirst("identifier");
-      providerResponse = ppmsClient.providersForId(identifier);
-    } else if (parameters.containsKey("name")) {
-      String name = parameters.getFirst("name");
-      providerResponse = ppmsClient.providersForName(name);
-    } else {
-      String familyName = parameters.getFirst("family");
-      String givenName = parameters.getFirst("given");
-      providerResponse = ppmsClient.providersForName(familyName);
-      List<ProviderResponse.Value> providerResponseFiltered = new ArrayList<>();
-      for (ProviderResponse.Value val : providerResponse.value()) {
-        if (StringUtils.containsIgnoreCase(val.name(), givenName)) {
-          providerResponseFiltered.add(val);
-        }
+  private Pair<List<PractitionerRoleWrapper>, Integer> search(
+          MultiValueMap<String, String> parameters) {
+      if (parameters.containsKey("identifier")) {
+        return searchIdentifier(parameters);
+      } else {
+        return searchFamilyGiven(parameters);
       }
-      if (providerResponseFiltered.size() == 0) {
-        throw new PpmsClient.PpmsException(
-            "No family name and given name found for combination '"
-                + familyName
-                + "' and '"
-                + givenName
-                + "'.");
-      }
-      providerResponse.value(providerResponseFiltered);
     }
+
+  /** Logic for search by Family and Given. */
+  private Pair<List<PractitionerRoleWrapper>, Integer> searchFamilyGiven(
+          MultiValueMap<String, String> parameters) {
+    String familyName = parameters.getFirst("family");
+    String givenName = parameters.getFirst("given");
+    ProviderResponse providerResponse = ppmsClient.providersForName(familyName);
+    int page = Integer.parseInt(parameters.getOrDefault("page", singletonList("1")).get(0));
+    int count = Integer.parseInt(parameters.getOrDefault("_count", singletonList("15")).get(0));
+    int fromIndex = Math.min((page - 1) * count, providerResponse.value().size());
+    int toIndex = Math.min((fromIndex + count), providerResponse.value().size());
+    /** Retrieve a list of providerResponse from PPMS using familyName. */
+    List<ProviderResponse.Value> providerResponseUnfilteredPages =
+            providerResponse.value().subList(fromIndex, toIndex);
+    /** Remove any providerResponse that doesn't contain the givenName. */
+    List<ProviderResponse.Value> providerResponsePages = new ArrayList<>();
+    for (int i = 0; i < providerResponseUnfilteredPages.size(); i++) {
+      if (StringUtils.containsIgnoreCase(
+              providerResponseUnfilteredPages.get(i).name(), givenName)) {
+        providerResponsePages.add(providerResponse.value().get(i));
+      }
+    }
+    /** Using providerResponse, retrieve a list of providerContactsResponse from PPMS. */
+    List<ProviderContactsResponse> providerContactsResponsePages =
+            providerResponsePages
+                    .parallelStream()
+                    .map(
+                            prv -> {
+                              return ppmsClient.providerContactsForId(prv.providerIdentifier().toString());
+                            })
+                    .collect(Collectors.toList());
+
+    /** Using providerResponse, retrieve a list of providerSpecialtyResponse from PPMS. */
+    List<ProviderSpecialtiesResponse> providerSpecialtiesResponsePages =
+            providerResponsePages
+                    .parallelStream()
+                    .map(
+                            prv -> {
+                              return ppmsClient.providerSpecialtySearch(prv.providerIdentifier().toString());
+                            })
+                    .collect(Collectors.toList());
+    /**
+     * Wrap providerResponse and providerContacts together to create a list of Practitioner (FHIR).
+     */
+    List<PractitionerRoleWrapper> practitionerWrapperPages =
+            IntStream.range(0, providerContactsResponsePages.size())
+                    .parallel()
+                    .mapToObj(
+                            i ->
+                                    practitionerRoleWrapperBuilder(
+                                            providerContactsResponsePages.get(i), providerResponsePages.get(i), providerSpecialtiesResponsePages.get(i)))
+                    .collect(Collectors.toList());
+    return Pair.of(practitionerWrapperPages, providerResponse.value().size());
+  }
+
+  /** Logic for search by Identifier. */
+  private Pair<List<PractitionerRoleWrapper>, Integer> searchIdentifier(
+          MultiValueMap<String, String> parameters) {
+    PractitionerRoleWrapper.PractitionerRoleWrapperBuilder practitionerRoleWrapper =
+            new PractitionerRoleWrapper.PractitionerRoleWrapperBuilder();
+    String identifier = parameters.getFirst("identifier");
+    ProviderResponse providerResponse = ppmsClient.providersForId(identifier);
     String providerIdentifier = providerResponse.value().get(0).providerIdentifier().toString();
     ProviderContactsResponse providerContactsResponse =
-        ppmsClient.providerContactsForId(
-            providerResponse.value().get(0).providerIdentifier().toString());
-    ProviderSpecialtiesResponse providerSpecialty =
-        ppmsClient.providerSpecialtySearch(providerIdentifier);
-    return PractitionerRoleWrapper.builder()
-        .providerContactsResponse(providerContactsResponse)
-        .providerResponse(providerResponse)
-        .providerSpecialtiesResponse(providerSpecialty)
-        .build();
+            ppmsClient.providerContactsForId(providerIdentifier);
+    ProviderSpecialtiesResponse providerSpecialtiesResponse = ppmsClient.providerSpecialtySearch(providerIdentifier);
+    return Pair.of(
+            singletonList(
+                    practitionerRoleWrapper
+                            .providerContactsResponse(providerContactsResponse)
+                            .providerResponse(providerResponse)
+                            .providerSpecialtiesResponse(providerSpecialtiesResponse)
+                            .build()),
+            1);
   }
 
   /** Search by family and given. */
