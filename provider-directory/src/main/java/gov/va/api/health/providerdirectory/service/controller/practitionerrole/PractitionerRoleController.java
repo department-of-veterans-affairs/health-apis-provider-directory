c9d939be235778d5fb9330a94d7c644f02b59f52
jjennings <joshua.jennings@libertyits.com>
2019-10-08 08:57:45 -0400

    Added the $validate and corresponding test

--- provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/practitionerrole/PractitionerRoleController.java
+++ provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/controller/practitionerrole/PractitionerRoleController.java
@@ -13,6 +13,8 @@ import gov.va.api.health.providerdirectory.service.controller.Bundler;
 import gov.va.api.health.providerdirectory.service.controller.Bundler.BundleContext;
 import gov.va.api.health.providerdirectory.service.controller.PageLinks.LinkConfig;
 import gov.va.api.health.providerdirectory.service.controller.Parameters;
+import gov.va.api.health.providerdirectory.service.controller.Validator;
+import gov.va.api.health.stu3.api.resources.OperationOutcome;
 import gov.va.api.health.stu3.api.resources.PractitionerRole;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,8 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -215,6 +219,15 @@ public class PractitionerRoleController {
                 .providerSpecialtiesResponse(providerSpecialtiesResponse)
                 .build()),
         1);
+  }
+
+  /** Hey, this is a validate endpoint. It validates. */
+  @PostMapping(
+    value = "/$validate",
+    consumes = {"application/json", "application/json+fhir", "application/fhir+json"}
+  )
+  public OperationOutcome validate(@RequestBody PractitionerRole.Bundle bundle) {
+    return Validator.create().validate(bundle);
   }
 
   public interface Transformer extends Function<PractitionerRoleWrapper, PractitionerRole> {}
