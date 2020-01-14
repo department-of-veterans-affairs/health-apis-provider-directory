c93d871a7d958974a762fe5974469fac418bb016
jblefkove-va <46896876+jblefkove-va@users.noreply.github.com>
2020-01-14 15:00:30 -0500

    SSS-111 Restore VLER (#56)

--- provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/client/RestVlerClient.java
+++ provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/client/RestVlerClient.java
@@ -1,25 +1,30 @@
 package gov.va.api.health.providerdirectory.service.client;
 
-import static java.util.Arrays.asList;
-
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.providerdirectory.service.AddressResponse;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
-import java.util.Collections;
-import java.util.concurrent.Callable;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /** REST implementation of VLER client. */
@@ -31,39 +36,29 @@ public class RestVlerClient implements VlerClient {
 
   private final String privateKey;
 
-  private final RestTemplate restTemplate;
+  private final String keyStorePath;
+
+  private final String keyStorePassword;
 
   /** Autowired constructor. */
   public RestVlerClient(
       @Value("${vler.url}") String baseUrl,
       @Value("${vler.key.public}") String publicKey,
       @Value("${vler.key.private}") String privateKey,
-      @Autowired RestTemplate restTemplate) {
+      @Value("${ssl.key-store}") String keyStorePath,
+      @Value("${ssl.key-store-password}") String keyStorePassword) {
     this.baseUrl = baseUrl.endsWith("/") ? baseUrl : baseUrl + "/";
     this.publicKey = publicKey;
     this.privateKey = privateKey;
-    this.restTemplate = restTemplate;
+    this.keyStorePath = keyStorePath;
+    this.keyStorePassword = keyStorePassword;
   }
 
-  @SneakyThrows
-  private static AddressResponse handleVlerExceptions(
-      String message, Callable<AddressResponse> callable) {
-    AddressResponse response;
-    try {
-      response = callable.call();
-    } catch (HttpClientErrorException.NotFound e) {
-      throw new Exceptions.NotFound(message, e);
-    } catch (HttpClientErrorException.BadRequest e) {
-      throw new Exceptions.BadRequest(message, e);
-    } catch (HttpStatusCodeException e) {
-      throw new Exceptions.SearchFailed(message, e);
-    } catch (Exception e) {
-      throw new Exceptions.VlerException(message, e);
+  private static String fileOrClasspath(String path) {
+    if (StringUtils.startsWith(path, "file:") || StringUtils.startsWith(path, "classpath:")) {
+      return path;
     }
-    if (response == null) {
-      throw new Exceptions.VlerException(message + ", no VLER response");
-    }
-    return response;
+    throw new IllegalArgumentException("Expected file or classpath resources. Got " + path);
   }
 
   @SneakyThrows
@@ -87,46 +82,49 @@ public class RestVlerClient implements VlerClient {
    * Calls the VLER Direct API by email address search. This will always return an unfiltered body.
    */
   @Override
+  @SneakyThrows
   public AddressResponse endpointByAddress(String address) {
-    return handleVlerExceptions(
-        address,
-        () -> {
-          String url =
-              UriComponentsBuilder.fromHttpUrl(baseUrl + "/direct/addresses").build().toUriString();
-          String dateString =
-              DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss z").format(ZonedDateTime.now());
-          HttpHeaders headers = new HttpHeaders();
-          headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-          headers.setContentType(MediaType.APPLICATION_JSON);
-          headers.add("Authorization", authHeader(url, dateString));
-          headers.add("Date", dateString);
+    String urlPath =
+        UriComponentsBuilder.fromHttpUrl(baseUrl + "/direct/addresses").build().toUriString();
+    return endpointByAddress(address, urlPath, new URL(urlPath));
+  }
 
-          //          ResponseEntity<AddressResponse> entity =
-          //              restTemplate.exchange(
-          //                  url, HttpMethod.GET, new HttpEntity<>(headers),
-          // AddressResponse.class);
-          //          return entity.getBody();
+  AddressResponse endpointByAddress(String address, String urlPath, URL url) {
+    String dateString =
+        DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss z").format(ZonedDateTime.now());
+    HttpURLConnection conn = null;
+    try {
+      conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestProperty("Authorization", authHeader(urlPath, dateString));
+      conn.setRequestProperty("Date", dateString);
+      conn.setRequestProperty("Accept", "application/json");
+      conn.setRequestProperty("content-type", "application/json");
+      conn.setRequestMethod("GET");
+      String result = StreamUtils.copyToString(conn.getInputStream(), StandardCharsets.UTF_8);
+      return JacksonConfig.createMapper().readValue(result, AddressResponse.class);
+    } catch (Exception e) {
+      throw new Exceptions.VlerException(address, e);
+    } finally {
+      if (conn != null) {
+        conn.disconnect();
+      }
+    }
+  }
 
-          // Return fake data
-          return AddressResponse.builder()
-              .contacts(
-                  asList(
-                      AddressResponse.Contacts.builder()
-                          .displayName("Pilot, Test")
-                          .emailAddress("test.pilot@test2.direct.va.gov")
-                          .uid("test.pilot")
-                          .givenName("Test")
-                          .surname("Pilot")
-                          .officeCityState("Indianapolis, IN")
-                          .companyName("Test Company")
-                          .departmentNumber("Test Department")
-                          .mobile("123-456-7890")
-                          .telephoneNumber("987-654-3210")
-                          .title("Testing Analyzer")
-                          .commonName("Test Pilot")
-                          .facility("Test Facility")
-                          .build()))
-              .build();
-        });
+  /** Initialize SSL. */
+  @SneakyThrows
+  @EventListener(ApplicationStartedEvent.class)
+  public void initSsl() {
+    try (InputStream keystoreInputStream =
+        ResourceUtils.getURL(fileOrClasspath(keyStorePath)).openStream()) {
+      KeyStore ts = KeyStore.getInstance("JKS");
+      ts.load(keystoreInputStream, keyStorePassword.toCharArray());
+      TrustManagerFactory trustManagerFactory =
+          TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init(ts);
+      SSLContext sslContext = SSLContext.getInstance("TLS");
+      sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
+      HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+    }
   }
 }
--- provider-directory/src/test/java/gov/va/api/health/providerdirectory/service/client/RestVlerClientTest.java
+++ provider-directory/src/test/java/gov/va/api/health/providerdirectory/service/client/RestVlerClientTest.java
@@ -2,53 +2,89 @@ package gov.va.api.health.providerdirectory.service.client;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.providerdirectory.service.AddressResponse;
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.ReflectionUtils;
 
 public final class RestVlerClientTest {
   @Test
-  @SuppressWarnings("unchecked")
-  public void endpointByAddressTest() {
-    ResponseEntity<AddressResponse> response = mock(ResponseEntity.class);
-    when(response.getBody()).thenReturn(AddressResponse.builder().build());
-    RestTemplate restTemplate = mock(RestTemplate.class);
-    when(restTemplate.exchange(
-            eq("http://foo.bar/direct/addresses"),
-            eq(HttpMethod.GET),
-            any(HttpEntity.class),
-            eq(AddressResponse.class)))
-        .thenReturn(response);
-    RestVlerClient client =
-        new RestVlerClient("http://foo.bar/", "testKey", "testKey", restTemplate);
-    assertThat(client.endpointByAddress("test"))
-        .isEqualTo(
-            AddressResponse.builder()
-                .contacts(
-                    asList(
-                        AddressResponse.Contacts.builder()
-                            .displayName("Pilot, Test")
-                            .emailAddress("test.pilot@test2.direct.va.gov")
-                            .uid("test.pilot")
-                            .givenName("Test")
-                            .surname("Pilot")
-                            .officeCityState("Indianapolis, IN")
-                            .companyName("Test Company")
-                            .departmentNumber("Test Department")
-                            .mobile("123-456-7890")
-                            .telephoneNumber("987-654-3210")
-                            .title("Testing Analyzer")
-                            .commonName("Test Pilot")
-                            .facility("Test Facility")
-                            .build()))
-                .build());
+  @SneakyThrows
+  public void endpointByAddress() {
+    String baseUrl = "http://foo.bar/";
+    AddressResponse addressResponse =
+        AddressResponse.builder()
+            .contacts(
+                asList(
+                    AddressResponse.Contacts.builder()
+                        .displayName("Pilot, Test")
+                        .emailAddress("test.pilot@test2.direct.va.gov")
+                        .uid("test.pilot")
+                        .givenName("Test")
+                        .surname("Pilot")
+                        .officeCityState("Indianapolis, IN")
+                        .companyName("Test Company")
+                        .departmentNumber("Test Department")
+                        .mobile("123-456-7890")
+                        .telephoneNumber("098-765-4321")
+                        .title("Testing Analyzer")
+                        .commonName("Test Pilot")
+                        .facility("Test Facility")
+                        .build()))
+            .build();
+
+    HttpURLConnection connection = mock(HttpURLConnection.class);
+    when(connection.getInputStream())
+        .thenReturn(
+            new ByteArrayInputStream(
+                JacksonConfig.createMapper()
+                    .writeValueAsString(addressResponse)
+                    .getBytes("UTF-8")));
+
+    // URL cannot be mocked, so its internal URL stream handler must be overridden
+    URL url = new URL(baseUrl);
+    Field handlerField = URL.class.getDeclaredField("handler");
+    ReflectionUtils.makeAccessible(handlerField);
+    handlerField.set(url, new MockUrlStreamHandler(connection));
+
+    RestVlerClient client = new RestVlerClient(baseUrl, "unused", "unused", "unused", "unused");
+    assertThat(client.endpointByAddress("test", baseUrl, url)).isEqualTo(addressResponse);
+  }
+
+  @SneakyThrows
+  @Test(expected = Exceptions.VlerException.class)
+  public void endpointByAddressError() {
+    String baseUrl = "http://foo.bar/";
+    HttpURLConnection connection = mock(HttpURLConnection.class);
+    when(connection.getInputStream()).thenThrow(new RuntimeException());
+
+    URL url = new URL(baseUrl);
+    Field handlerField = URL.class.getDeclaredField("handler");
+    ReflectionUtils.makeAccessible(handlerField);
+    handlerField.set(url, new MockUrlStreamHandler(connection));
+
+    RestVlerClient client = new RestVlerClient(baseUrl, "unused", "unused", "unused", "unused");
+    client.endpointByAddress("test", baseUrl, url);
+  }
+
+  @AllArgsConstructor
+  private static final class MockUrlStreamHandler extends URLStreamHandler {
+    final HttpURLConnection connection;
+
+    @Override
+    protected URLConnection openConnection(URL u) {
+      return connection;
+    }
   }
 }
