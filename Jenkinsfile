48fc0a1918e83bf823ebbc4d3cae0d44e4556d44
Adrian Garcia <that.guy.is.it@gmail.com>
2021-07-16 15:27:37 -0400

    Reset (#58)

--- Jenkinsfile
+++
@@ -1,3 +0,0 @@
-standardMavenPipeline {
-    healthApisMavenImage='vasdvp/health-apis-maven:3.6-jdk-12'
-}
--- provider-directory/src/main/java/gov/va/api/health/providerdirectory/service/client/VlerClient.java
+++
@@ -1,7 +0,0 @@
-package gov.va.api.health.providerdirectory.service.client;
-
-import gov.va.api.health.providerdirectory.service.AddressResponse;
-
-public interface VlerClient {
-  AddressResponse endpointByAddress(String searchFunction);
-}
