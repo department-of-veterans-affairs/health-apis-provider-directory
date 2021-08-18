85133c0a3026f57e8ae0e83af5368e3943f416f9
Taylor Rassmann <taylor.rassmann@libertyits.com>
2021-08-18 10:30:48 -0400

    Updated to use non bundle class for responses.

--- provider-directory-tests/src/test/java/gov/va/api/health/providerdirectory/tests/DataQueryIT.java
+++ provider-directory-tests/src/test/java/gov/va/api/health/providerdirectory/tests/DataQueryIT.java
@@ -35,25 +35,31 @@ public class DataQueryIT {
 
   @Test
   void doNotReplaceNamingSystemUrl() {
-    final String FULL_FHIR_STRING =
+    final String FHIR_STRING =
         "https://api.va.gov/services/fhir/v0/r4/NamingSystem/va-clinic-identifier";
-    final String FHIR_STRING = "services/fhir/v0/r4/NamingSystem/";
+
     var ids = systemDefinition().publicIds();
+
     var locationResponse =
-        doGet(null, "r4/Location/" + ids.location(), 200).expectValid(Location.Bundle.class);
-    assertThat(locationResponse.identifier().system()).contains(FULL_FHIR_STRING);
+        doGet(null, "r4/Location/" + ids.location(), 200).expectValid(Location.class);
+    assertThat(locationResponse.identifier().size()).isEqualTo(1);
+    assertThat(locationResponse.identifier().get(0).system()).contains(FHIR_STRING);
+
     var organizationResponse =
-        doGet(null, "r4/Organization/" + ids.location(), 200)
-            .expectValid(Organization.Bundle.class);
-    assertThat(organizationResponse.identifier().system()).contains(FULL_FHIR_STRING);
+        doGet(null, "r4/Organization/" + ids.location(), 200).expectValid(Organization.class);
+    assertThat(organizationResponse.identifier().size()).isEqualTo(1);
+    assertThat(organizationResponse.identifier().get(0).system()).contains(FHIR_STRING);
+
     var practitionerResponse =
-        doGet(null, "r4/Practitioner/" + ids.location(), 200)
-            .expectValid(Practitioner.Bundle.class);
-    assertThat(practitionerResponse.identifier().system()).contains(FULL_FHIR_STRING);
+        doGet(null, "r4/Practitioner/" + ids.location(), 200).expectValid(Practitioner.class);
+    assertThat(practitionerResponse.identifier().size()).isEqualTo(1);
+    assertThat(practitionerResponse.identifier().get(0).system()).contains(FHIR_STRING);
+
     var practitionerRoleResponse =
         doGet(null, "r4/PractitionerRole/" + ids.location(), 200)
-            .expectValid(PractitionerRole.Bundle.class);
-    assertThat(practitionerRoleResponse.identifier().system()).contains(FULL_FHIR_STRING);
+            .expectValid(PractitionerRole.class);
+    assertThat(practitionerRoleResponse.identifier().size()).isEqualTo(1);
+    assertThat(practitionerRoleResponse.identifier().get(0).system()).contains(FHIR_STRING);
   }
 
   @ParameterizedTest
