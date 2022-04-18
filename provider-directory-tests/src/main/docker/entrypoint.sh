215056461104db8b2d5c0141ee66c700e99a8cc3
agarcia-va <Garcia_Adrian@bah.com>
2022-04-18 15:13:53 -0400

    Removed K8S_ variables from entrypoint.sh

--- provider-directory-tests/src/main/docker/entrypoint.sh
+++ provider-directory-tests/src/main/docker/entrypoint.sh
@@ -5,12 +5,6 @@ set -euo pipefail
 if [ -z "${SENTINEL_BASE_DIR:-}" ]; then SENTINEL_BASE_DIR=/sentinel; fi
 cd $SENTINEL_BASE_DIR
 
-test -n "${K8S_ENVIRONMENT}"
-test -n "${K8S_LOAD_BALANCER}"
-
-if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="$K8S_ENVIRONMENT"; fi
-if [ -z "${SENTINEL_URL:-}" ]; then SENTINEL_URL="https://${K8S_LOAD_BALANCER}"; fi
-
 java-tests \
   --module-name "provider-directory-tests" \
   --regression-test-pattern ".*IT\$" \
