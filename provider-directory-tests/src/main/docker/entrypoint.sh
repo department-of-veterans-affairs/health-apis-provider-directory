805a02963db3fc4f656d6e08e1baf12c7c4cda16
agarcia-va <98479130+agarcia-va@users.noreply.github.com>
2022-04-19 14:52:30 -0400

    API-15094 remove unsupported environments (#84)

--- provider-directory-tests/src/main/docker/entrypoint.sh
+++ provider-directory-tests/src/main/docker/entrypoint.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 if [ -z "${SENTINEL_BASE_DIR:-}" ]; then SENTINEL_BASE_DIR=/sentinel; fi
 cd $SENTINEL_BASE_DIR
 
-test -n "${K8S_ENVIRONMENT}"
-test -n "${K8S_LOAD_BALANCER}"
+test -n "${DEPLOYMENT_ENVIRONMENT}"
+test -n "${DEPLOYMENT_TEST_HOST}"
 
-if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="$K8S_ENVIRONMENT"; fi
-if [ -z "${SENTINEL_URL:-}" ]; then SENTINEL_URL="https://${K8S_LOAD_BALANCER}"; fi
+if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="$DEPLOYMENT_ENVIRONMENT"; fi
+if [ -z "${SENTINEL_URL:-}" ]; then SENTINEL_URL="${DEPLOYMENT_TEST_PROTOCOL}://${DEPLOYMENT_TEST_HOST}:${DEPLOYMENT_TEST_PORT}"; fi
 
 java-tests \
   --module-name "provider-directory-tests" \
